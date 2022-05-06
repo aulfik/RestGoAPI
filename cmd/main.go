e0e370943ce8fddf802b5400d597c80edff72f28
aulfik <auautori@gmail.com>
2022-05-06 22:30:14 +0700

    Working

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
@@ -29,12 +31,31 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Successfully connected!")
-	// r := gin.Default()
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "Hello World!",
-	// 	})
-	// })
-	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		var (
+			username string
+			email    string
+		)
+		rows, err := db.Query("select * from user_data")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			err := rows.Scan(&username, &email)
+			if err != nil {
+				log.Fatal(err)
+			}
+			c.JSON(200, gin.H{
+				"username": username,
+				"email":    email,
+			})
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
